70f01bc0ec7d27464a98dc5e181a8191290dae1b
leoebc1 <92554228+leoebc1@users.noreply.github.com>
2025-02-12 20:49:28 -0300

    Add files via upload

---
+++ Pokemon.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pokemon;
+
+/**
+ *
+ * @author LEONARDOESLABAOBARBO
+ */
+public class Pokemon {
+
+    private String nome;
+    private String tipo;
+    private int nivel;
+
+    public Pokemon(String nome, String tipo, int nivel) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.nivel = nivel;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public void exibirInfo() {
+        System.out.println("Nome: " + nome);
+
+    }
+}
